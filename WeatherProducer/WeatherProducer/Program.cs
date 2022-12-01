7c7cbd09f650a52639b26e7f8bc15f3ab4e9a003
Cesar Lopez <cesarlerdeut@gmail.com>
2022-12-01 22:32:33 +0100

    Initial Commit

---
+++ WeatherProducer/WeatherProducer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig()
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+try
+{
+    string? state;
+    
+    Console.WriteLine("Type State:");
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = await producer.ProduceAsync(
+            "weather-topic",
+            new Message<Null, string>()
+            {
+                Value = JsonConvert.SerializeObject(new Weather(state, 70))
+            });
+        
+        Console.WriteLine(response.Value);
+        Console.WriteLine("Type State:");
+    }
+}
+catch (ProduceException<Null, string> e)
+{
+    Console.WriteLine(e.Message);
+}
+
+public record Weather(string State, int Temperature);
